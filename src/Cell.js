f511c9883239348e0119abdd686e95a9e40b721c
ianvei <iamianveilleux@gmail.com>
2022-07-15 11:29:21 -0400

    styled gameboard

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-const Cell = ( { x, y, isMine, neighbors, testFunc, isRevealed }) => {
-    let [flag, setFlag] = useState(false)
+const Cell = ( { x, y, isMine, neighbors, testFunc, isRevealed, contextfunc, flag }) => {
+    // let [flag, setFlag] = useState(false)
 
     const onClick = () => {
         console.log(x, y, isMine, neighbors, isRevealed)
@@ -9,18 +9,19 @@ const Cell = ( { x, y, isMine, neighbors, testFunc, isRevealed }) => {
         
     }
 
-    const onContextMenu = (e) => {
-        e.preventDefault();
-        console.log('context')
-        setFlag(flag = !flag)
-        console.log(flag)
-        console.log(isRevealed)
-        console.log(neighbors)
-    }
+    // const onContextMenu = (e) => {
+    //     e.preventDefault();
+    //     console.log('context')
+    //     setFlag(flag = !flag)
+    //     console.log(flag)
+    //     console.log(isRevealed)
+    //     console.log(neighbors)
+    // }
     return (
-        <div className={isRevealed? 'cell revealed' : 'cell' } onClick={testFunc} onContextMenu={onContextMenu}>{flag? 'flag' : isRevealed&&isMine? 'mine' : isRevealed&&neighbors? neighbors: '' }</div>
+        <div className={isRevealed? `cell revealed neighbor${neighbors}` : 'cell' } onClick={testFunc} onContextMenu={contextfunc}> {flag? 'flag' : isRevealed&&isMine? 'M' : isRevealed&&neighbors? neighbors: '' } </div>
         // {flag? 'flag' : isMine ? 'mine' : isRevealed? "revealed ": neighbors }
     )
+    //isMine? 'mine' : 
 }
 
 export default Cell
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -8,6 +8,21 @@ const GameBoard = () => {
         mines: 10,
     })
     const [boardData, setBoardData] = useState([])
+    const [revealedCells, setRevealedCells] = useState(0);
+    const [timer, setTimer] = useState(0);
+    const [finalScore, setFinalScore] = useState(0);
+    const [stopTime, setStopTime] = useState(false)
+
+    useEffect(() => {
+
+        if (stopTime) {
+            setTimer(timer)
+        } else {
+            setTimeout(() => setTimer(timer + 1), 1000);
+        }
+        
+        // update about every second
+    },[timer])
 
     useEffect(() => {
         setBoardData(boardArray())
@@ -107,7 +122,7 @@ const GameBoard = () => {
         if(oldBoard[i][j].isMine) {
             console.log('GAMEOVER')
             newBoard.map((row) => {return row.map((col) => {
-                console.log('bruh')
+                // setRevealedCells(revealedCells => revealedCells + 1)
                 return col.isRevealed = true;
             })})
             return setBoardData(newBoard);
@@ -118,13 +133,15 @@ const GameBoard = () => {
             if ((newBoard[i][j].x + iShift) < 0 || (newBoard[i][j].x + iShift) >= dimension.width || (newBoard[i][j].y + jShift) < 0 || ((newBoard[i][j].y + jShift) >= dimension.height)) { // not in range
                 return
             }
-            if(newBoard[i+iShift][j+jShift].neighbors) { 
-                newBoard[i+iShift][j+jShift].isRevealed = true;
-                return;
-            }
             if(newBoard[i+iShift][j+jShift].isMine) { // bomb
                 return;
             }
+            if(newBoard[i+iShift][j+jShift].neighbors) {
+                setRevealedCells(oldRevealedCells => oldRevealedCells + 1) 
+                newBoard[i+iShift][j+jShift].isRevealed = true;
+                return;
+            }
+            
             if (newBoard[i+iShift][j+jShift].isRevealed) { // already revealed, no need to check again
                 return;
             }
@@ -132,29 +149,114 @@ const GameBoard = () => {
             setBoardData(newBoard);
             i = i+iShift;
             j = j+jShift;
-            floodFill(i, 0, j, 1, newBoard);
-            floodFill(i, 0, j, -1, newBoard);
-            floodFill(i, 1, j, 0, newBoard);
-            floodFill(i, -1, j, 0, newBoard);
-            floodFill(i, 1, j, 1, newBoard);
-            floodFill(i, -1, j, -1, newBoard);
-            floodFill(i, 1, j, -1, newBoard);
-            floodFill(i, -1, j, 1, newBoard);      
+
+            setTimeout(() => floodFill(i, 0, j, 1, newBoard), 30);
+            setTimeout(() => floodFill(i, 0, j, -1, newBoard), 30);
+            setTimeout(() => floodFill(i, 1, j, 0, newBoard), 30);
+            setTimeout(() => floodFill(i, -1, j, 0, newBoard), 30);
+            setTimeout(() => floodFill(i, 1, j, 1, newBoard), 30);
+            setTimeout(() => floodFill(i, -1, j, -1, newBoard), 30);
+            setTimeout(() => floodFill(i, 1, j, -1, newBoard), 30);
+            setTimeout(() => floodFill(i, -1, j, 1, newBoard), 30);
+
+
+
+
+
+            // floodFill(i, 0, j, 1, newBoard);
+            // floodFill(i, 0, j, -1, newBoard);
+            // floodFill(i, 1, j, 0, newBoard);
+            // floodFill(i, -1, j, 0, newBoard);
+            // floodFill(i, 1, j, 1, newBoard);
+            // floodFill(i, -1, j, -1, newBoard);
+            // floodFill(i, 1, j, -1, newBoard);
+            // floodFill(i, -1, j, 1, newBoard);      
         }
 
         newBoard[i][j].isRevealed = true;
+        // setRevealedCells(oldRevealedCells => oldRevealedCells + 1)
+
+        
+        // newBoard.map((row) => {return row.map((col) => {
+        //     if(col.isRevealed) {
+        //         setRevealedCells(revealedCells => revealedCells + 1)
+        //     }
+        // })})
+
+        
+
+        checkWin(newBoard);
+        // console.log(`revealed cells: ${revealedCells}`)
         return setBoardData(newBoard);
     }
 
+    const contextFunc = (col, boardData, e) => {
+        e.preventDefault();
+        const newBoard = [...boardData];
+        let flagCount = 0;
+        col.isFlag = !col.isFlag;
+
+        newBoard.map((row) => {return row.map((col) => {
+            if (col.flag && col.isMine) {
+                flagCount += 1;
+            }     
+        })})
+
+        // if (flagCount === 10) {
+        //     console.log('you win!')
+        //     newBoard.map((row) => {return row.map((col) => {
+        //         col.isRevealed = true;
+        //     })})
+        // }
+        // newBoard.map((row) => {return row.map((col) => {
+        //     if(col.isRevealed) {
+        //         setRevealedCells(revealedCells => revealedCells + 1)
+        //     }
+        // })})
+        console.log(revealedCells)
+        checkWin(newBoard);
+        return setBoardData(newBoard);
+    }
+
+    const checkWin = (newBoard) => {
+        // if(((dimension.width * dimension.height ) - revealedCells) === dimension.mines) {
+        //     console.log('you win!')
+        // }
+        let count = 0
+        newBoard.map((row) => {return row.map((col) => {
+                if(col.isRevealed) {
+                    count += 1
+                }
+            })})
+        
+        if(((dimension.width * dimension.height ) - count) === dimension.mines) {
+            console.log(`it took ${timer} seconds to win! congratulations!`)
+            setStopTime(true)
+            setFinalScore(timer + 1)
+        }
+
+        console.log(count)
+    }
+
     return(
-        <div className="game-cont">
+        <div className="main">
+            {/* <div className="timer">{timer}</div>
+            <div className="score">{stopTime? finalScore : ''}</div> */}
+            <div className="score">
+                <div className="flags">10</div>
+                <div className="smile"></div>
+                <div className="timer">{stopTime? finalScore: timer}</div>
+            </div>
+            <div className="game-cont">
             {boardData.map((row) => {return row.map((col) => {   
                         return(
-                            <Cell x={col.x} y={col.y} isMine={col.isMine} neighbors={col.neighbors} isRevealed={col.isRevealed} key={col.x + col.y} testFunc={() => floodFill(col.x, 0, col.y, 0, boardData)}/>
+                            <Cell x={col.x} y={col.y} isMine={col.isMine} flag={col.isFlag} contextfunc={(e) => contextFunc(col, boardData, e)}neighbors={col.neighbors} isRevealed={col.isRevealed} key={col.x + col.y} testFunc={() => floodFill(col.x, 0, col.y, 0, boardData)}/>
                         )
                     })}
                 )}
         </div>
+        </div>
+        
     )
     
 }
