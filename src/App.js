26896b4e79f6853e362aad02e87af2dd26f2941d
ianvei <iamianveilleux@gmail.com>
2022-07-16 17:25:52 -0400

    finished game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import GameBoard from './GameBoard';
+import windowsLogo from './images/windowlogo.png'
 
 function App() {
   return (
     <div className="App">
-      <GameBoard />
+      <div className="main-screen">
+        <GameBoard />
+      </div>
+      <div className="footer">
+        <div className="start-button">
+            <img src={windowsLogo} alt="windows logo" />
+            <p>Start</p>
+        </div>
+    </div>
     </div>
   );
 }
