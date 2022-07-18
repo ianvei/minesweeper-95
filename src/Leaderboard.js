b1732a603c3f64db2249828251298d5ebb004bfe
ianvei <iamianveilleux@gmail.com>
2022-07-18 15:20:34 -0400

    initialized router

---
+++ src/Leaderboard.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+const Leaderboard = ( ) => {
+    return (
+        <h1>Leaderboard</h1>
+    )
+}
+
+export default Leaderboard
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,20 @@
 import './App.css';
 import GameBoard from './GameBoard';
 import windowsLogo from './images/windowlogo.png'
+import Leaderboard from './Leaderboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="main-screen">
-        <GameBoard />
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<GameBoard />} />
+              <Route path='/leaderboard' element={<Leaderboard />}/>
+          </Routes>
+        </BrowserRouter>
+        {/* <GameBoard /> */}
       </div>
       <div className="footer">
         <div className="start-button">
